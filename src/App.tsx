71ecf2cd6a24300c9ae5a0c03ba00e67b7e23a15
aristoersapta@gmail.com <aristoersapta@gmail.com>
2023-07-18 14:26:38 +0700

    [main] add mobile style and css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/styles/HankenGroteskFont.css";
+import "./App.css";
+import IconReaction from "./assets/images/icon-reaction.svg";
+import IconMemory from "./assets/images/icon-memory.svg";
+import IconVerbal from "./assets/images/icon-verbal.svg";
+import IconVisual from "./assets/images/icon-visual.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <header className="App-header">
+        <h3 className="header-title">Your Result</h3>
+        <div className="score-wrapper">
+          <h1 className="score">76</h1>
+          <p className="score-total">of 100</p>
+        </div>
+        <h2 className="score-level">Great</h2>
+        <p className="score-info">
+          Your performance exceed 65% of the people conducting the test here!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main role="main" className="App">
+        <h3>Summary</h3>
+        <section about="score for each category">
+          <div className="score-category-wrapper" id="category-reaction">
+            <figure className="category-detail">
+              <img
+                src={IconReaction}
+                className="category-icon"
+                alt="reaction-category-icon"
+              />
+              <figcaption className="category-name">Reaction</figcaption>
+            </figure>
+            <div className="category-score">
+              <span>80</span>
+              <span>/</span>
+              <span>100</span>
+            </div>
+          </div>
+
+          <div className="score-category-wrapper" id="category-memory">
+            <figure className="category-detail">
+              <img
+                src={IconMemory}
+                className="category-icon"
+                alt="memory-category-icon"
+              />
+              <figcaption className="category-name">Memory</figcaption>
+            </figure>
+            <div className="category-score">
+              <span>92</span>
+              <span>/</span>
+              <span>100</span>
+            </div>
+          </div>
+
+          <div className="score-category-wrapper" id="category-verbal">
+            <figure className="category-detail">
+              <img
+                src={IconVerbal}
+                className="category-icon"
+                alt="verbal-category-icon"
+              />
+              <figcaption className="category-name">Verbal</figcaption>
+            </figure>
+            <div className="category-score">
+              <span>61</span>
+              <span>/</span>
+              <span>100</span>
+            </div>
+          </div>
+
+          <div className="score-category-wrapper" id="category-visual">
+            <figure className="category-detail">
+              <img
+                src={IconVisual}
+                className="category-icon"
+                alt="visual-category-icon"
+              />
+              <figcaption className="category-name">Visual</figcaption>
+            </figure>
+            <div className="category-score">
+              <span>73</span>
+              <span>/</span>
+              <span>100</span>
+            </div>
+          </div>
+        </section>
+        <button className="continue-btn">Continue</button>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
